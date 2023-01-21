030b883b048b40971dd020ec592ba754a01eed4a
MohanKumarR13 <123152359+MohanKumarR13@users.noreply.github.com>
2023-01-22 02:24:34 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,7 +1,7 @@
 
 public class Jenkins {
 		public static void main(String[] args) {
-		System.out.println("Study");	
+		System.out.println("Study") ;	
 		System.out.println("Salary");
 		System.out.println("Amont");
 		System.out.println("Expenses") ;
